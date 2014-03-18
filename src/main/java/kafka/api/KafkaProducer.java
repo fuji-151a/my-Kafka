62fc7e57a7279114517d6b15579107e70237b0fe
yuya <yuya@fuji-PC>
2014-03-18 23:56:04 +0900

    リファクタリング

--- src/main/java/kafka/api/KafkaProducer.java
+++ src/main/java/kafka/api/KafkaProducer.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.joda.time.DateTime;
+
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
@@ -22,6 +24,8 @@ public class KafkaProducer {
     private String topic;
     /** produce count. */
     private int produceCount = 0;
+    /** sleep time. */
+    private long sleep = 0;
 
     /**
      * 空のコンストラクタ.今のところ未定.
@@ -90,26 +94,77 @@ public class KafkaProducer {
     }
 
     /**
+     * set sleep time.
+     * @param time : millis
+     */
+    private void setSleepTime(long time) {
+        this.sleep = time;
+    }
+
+    /**
      * 指定した件数Produceを行う.
      * @param logData
      */
     public void countProduce(String logData) {
         for (int i = 0; i < produceCount; i++) {
-            produce(i + ":" + logData);
+            produce(currentTime() + " " + i + ":" + logData);
         }
     }
 
+    /**
+     * 無限にProduceを行う.
+     * @param logData
+     * @throws InterruptedException
+     */
+    public void contProduce(String logData) throws InterruptedException {
+        while (true) {
+            produce(currentTime() + " " + logData);
+            Thread.sleep(sleep * 1000);
+        }
+    }
+
+    /**
+     * 現在の時刻を[yyyy/MM/dd HH:mm:ss]形式で返す.
+     * このクラスに書く必要はないw 修正予定
+     * @return currentTime
+     */
+    private static String currentTime() {
+        DateTime dt = new DateTime();
+        return dt.toString("[yyyy/MM/dd HH:mm:ss]");
+    }
+
     public static void main(String[] args) {
-        if (args.length < 2) {
+        if (args.length < 3) {
             System.out.println("Please Input:");
             System.out.println("java -cp <jarFile> kafka.api.KafkaProducer "
-                    + "<propertiesFile> <produceNum>");
+                    + "<propertiesFile> <mode> <produceNum>");
+            System.out.println("mode:cont => 0");
+            System.out.println("mode:stat => 1");
         }
         String fileName = args[0];
+        int mode = Integer.parseInt(args[1]);
         String data = "Test Data";
         KafkaProducer kp = new KafkaProducer(fileName);
-        kp.setProduceCount(Integer.parseInt(args[1]));
-        kp.countProduce(data);
+        switch (mode) {
+            case 0:
+                kp.setSleepTime(1);
+                try {
+                    kp.contProduce(data);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case 1:
+                kp.setProduceCount(Integer.parseInt(args[2]));
+                kp.countProduce(data);
+                break;
+            default:
+                System.out.println("Please Input mode:");
+                System.out.println("mode:cont => 0");
+                System.out.println("mode:stat => 1");
+                break;
+        }
+
         System.out.println("System End");
     }
 }
